97f61ec0d0c31224f8c9acd250dd926a9cca4f90
SlavaOST <slavaost-it@mail.ru>
2024-03-24 21:33:07 +0300

    feat: add utils for Error

---
+++ src/utils/error-utils/error-utils.ts
@@ -0,0 +1,18 @@
+import axios, {AxiosError} from "axios";
+import {Dispatch} from "redux";
+
+import {AppStatus} from "../../common/types/commonTypes";
+import {SetAppErrorAT, SetAppStatusAT} from "../../state/reducers/appReducer/appReducer-types";
+import {setAppError, setAppStatus} from "../../state/reducers/appReducer/appReducer";
+
+
+export const baseErrorHandler = (e: Error | AxiosError, dispatch: Dispatch<SetAppErrorAT | SetAppStatusAT>) => {
+    const err = e as Error | AxiosError
+    if (axios.isAxiosError(err)) {
+        const error = err.response?.data
+            ? (err.response.data as ({ error: string })).error
+            : err.message
+        dispatch(setAppStatus({status: AppStatus.FAILED}))
+        dispatch(setAppError({error: error}))
+    }
+}
