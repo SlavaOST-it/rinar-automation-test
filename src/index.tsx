4b5417c89003f329053e00d8384c394faacc00d8
SlavaOST <slavaost-it@mail.ru>
2024-03-24 19:31:09 +0300

    feat: add browser router, store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
+import {Provider} from "react-redux";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './app/App';
+import {BrowserRouter} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
+import App from './app/App';
+import {store} from "./state/store/store";
+
+
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
