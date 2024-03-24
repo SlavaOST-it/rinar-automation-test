a41227f1b6f8bae95cc3a16bd8fe2e41e48830e7
SlavaOST <slavaost-it@mail.ru>
2024-03-24 21:33:36 +0300

    feat: add modal

---
+++ src/common/components/infoModal/ModalInfoBeer.tsx
@@ -0,0 +1,30 @@
+import React, {FC} from 'react';
+import {Modal} from "antd";
+
+
+type InfoModalProps = {
+    isOpen: boolean,
+
+    setIsOpenModal: (isOpen: boolean) => void,
+    children: React.ReactNode
+};
+
+export const ModalInfoBeer: FC<InfoModalProps> = ({isOpen, setIsOpenModal, children}) => {
+
+    const handleCancel = () => {
+        setIsOpenModal(false)
+    };
+
+    return (
+        <Modal
+            title="Пивной уголок: Информация о пиве"
+            open={isOpen}
+            footer={null}
+            width={600}
+            onCancel={handleCancel}
+
+        >
+            {children}
+        </Modal>
+    )
+};
