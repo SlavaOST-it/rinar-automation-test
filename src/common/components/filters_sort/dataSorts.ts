86faa9d30db94f2b0c53c3a87fa02befd1616b40
SlavaOST <slavaost-it@mail.ru>
2024-03-24 19:32:29 +0300

    feat: add data for filters and table

---
+++ src/common/components/filters_sort/dataSorts.ts
@@ -0,0 +1,89 @@
+export const sortAlcohol: Array<{ label: string, value: number }> = [
+    {
+        value: 100,
+        label: 'Любой %',
+    },
+    {
+        value: 1,
+        label: '0%',
+    },
+    {
+        value: 3,
+        label: 'До 3%',
+    },
+    {
+        value: 6,
+        label: 'До 6%',
+    },
+    {
+        value: 10,
+        label: 'До 10%',
+    },
+    {
+        label: 'До 20%',
+        value: 20,
+    },
+]
+
+export const sortCountry: Array<{ label: string, value: string }> = [
+    {
+        value: '',
+        label: 'Любая страна',
+    },
+    {
+        value: 'denmark',
+        label: 'Дания',
+    },
+    {
+        value: 'sweden',
+        label: 'Швеция',
+    },
+    {
+        value: 'belgium',
+        label: 'Бельгия',
+    },
+    {
+        value: 'spain',
+        label: 'Испания',
+    },
+    {
+        value: 'portugal',
+        label: 'Португалия',
+    },
+    {
+        value: 'ireland',
+        label: 'Ирландия',
+    },
+    {
+        value: 'luxembourg',
+        label: 'Люксенбург',
+    },
+    {
+        value: 'norway',
+        label: 'Норвегия',
+    },
+    {
+        value: 'finland',
+        label: 'Финляндия',
+    },
+    {
+        value: 'switzerland',
+        label: 'Швейцария',
+    },
+    {
+        value: 'czech',
+        label: 'Чехия',
+    },
+    {
+        value: 'italy',
+        label: 'Италия',
+    },
+    {
+        value: 'poland',
+        label: 'Польша',
+    },
+    {
+        value: 'malta',
+        label: 'Мальта',
+    },
+]
---
+++ src/common/components/table/dataTable.ts
@@ -0,0 +1,39 @@
+import {TableProps} from "antd";
+import {BeerType} from "../../types/beerType";
+
+
+interface ColumnI extends BeerType {
+    key: string;
+}
+
+export const columns: TableProps<ColumnI>['columns'] = [
+    {
+        title: 'Наименование',
+        dataIndex: 'title',
+        key: 'name',
+        width: '20%',
+    },
+    {
+        title: 'Алкоголь',
+        dataIndex: 'alchool',
+        key: 'alchool',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => {
+            const valueA = parseFloat(a.alchool.replace('%', ''));
+            const valueB = parseFloat(b.alchool.replace('%', ''));
+            return valueA - valueB;
+        },
+        width: '10%',
+    },
+    {
+        title: 'Страна',
+        dataIndex: 'country',
+        key: 'country',
+        width: '15%',
+    },
+    {
+        title: 'Описание',
+        dataIndex: 'description',
+        key: 'description',
+    },
+]
