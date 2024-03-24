8c846ba5a2950c2408453d45712ec82af80a4dae
SlavaOST <slavaost-it@mail.ru>
2024-03-24 21:31:41 +0300

    feat: add response types

--- src/api/beerApi.ts
+++ src/api/beerApi.ts
@@ -1,7 +1,5 @@
 import {instance} from "./apiConfig/instance";
-import {BeerType} from "../common/types/beerType";
-import {setAppStatus} from "../state/reducers/appReducer/appReducer";
-import {AppStatus} from "../common/types/commonTypes";
+import {BeerIsCountry, BeerType} from "../common/types/beerType";
 
 
 export const beerApi = {
@@ -10,20 +8,6 @@ export const beerApi = {
     },
 
     getBeersIsCountry(country: string){
-        return instance.get<BeerType[]>(`${country}`)
+        return instance.get<BeerIsCountry[]>(`${country}`)
     }
 }
-
-
-
-// fetchBeersIsCountry: create.asyncThunk (async function(_, {
-//     dispatch,
-//     rejectWithValue
-// }) {
-//     dispatch(setAppStatus({status: AppStatus.LOADING}))
-//     try {
-//
-//     }catch (e){
-//         const res = await beerApi.getCurrentBeer()
-//     }
-// }),
