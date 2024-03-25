aea7d71cc222d2e1c168f5c198e0e44c24ad4ee6
SlavaOST <slavaost-it@mail.ru>
2024-03-25 18:21:19 +0300

    feat: add colors constants

---
+++ src/common/styles/colors.ts
@@ -0,0 +1,7 @@
+
+export const primaryColor = '#464646';
+export const primaryColorHover = '#464646';
+export const accentColor = '#e8c408';
+export const borderTableColor = '#d7d7d7';
+export const headerSortHoverBgColor = '#ceaa0b';
+export const rowHoverBgColor = '#e5e5e5';
